5901598ba12425deb058e8a81090c0b78347396a
kobit <kobit@00b2b5fc-311a-0410-90ea-96fce9c84a0d>
2007-07-18 18:02:08 +0000

    [maven-release-plugin]  copy for tag tigase-xmltools-3.0.0
    git-svn-id: file:///home/svn/repos/tigase-xmltools@360 00b2b5fc-311a-0410-90ea-96fce9c84a0d

---
+++ tags/tigase-xmltools-3.0.0/src/main/java/tigase/xml/ElementFactory.java
@@ -0,0 +1,38 @@
+/*
+ * Tigase Jabber/XMPP XML Tools
+ * Copyright (C) 2004-2007 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+package tigase.xml;
+
+/**
+ * <code>ElementFactory</code> is inderface definition for factories
+ * creating proper instances of <code>Element</code> class or its extension.
+ * <p>
+ * Created: Mon Oct 25 21:47:45 2004
+ * </p>
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ */
+public interface ElementFactory {
+
+  Element elementInstance(String name, String cdata,
+    StringBuilder[] attNames, StringBuilder[] attValues);
+
+} // ElementFactory
