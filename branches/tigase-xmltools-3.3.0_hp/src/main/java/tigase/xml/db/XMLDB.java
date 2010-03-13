ec439e2b8b2acb5e7efd7d7452e66753950e8867
kobit <kobit@00b2b5fc-311a-0410-90ea-96fce9c84a0d>
2010-03-13 21:05:03 +0000

    License update
    git-svn-id: file:///home/svn/repos/tigase-xmltools@464 00b2b5fc-311a-0410-90ea-96fce9c84a0d

--- branches/tigase-xmltools-3.3.0_hp/src/main/java/tigase/xml/db/XMLDB.java
+++ branches/tigase-xmltools-3.3.0_hp/src/main/java/tigase/xml/db/XMLDB.java
@@ -1,19 +1,20 @@
 /*
- * Tigase Jabber/XMPP XML Tools
- * Copyright (C) 2004-2007 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * Tigase XMPP/Jabber XML Tools
+ * Copyright (C) 2004-2010 "Artur Hefczyc" <artur.hefczyc@tigase.org>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License.
+ * This program is licensed to HP, under Licensor's Intellectual Property Rights,
+ * a paid-up, worldwide, non-exclusive, non-transferable, perpetual license in the
+ * Source Code to use, make, reproduce (for archival and back-up purposes only),
+ * and prepare derivative works.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The right to prepare derivative works includes translations, localizations,
+ * adaptations, and compilations. HP shall own all right, title, and interest in
+ * any derivative works prepared by HP.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. Look for COPYING file in the top folder.
- * If not, see http://www.gnu.org/licenses/.
+ * HP shall use its standard forms in distributing Object Code.
+ *
+ * Licensor retains all right, title, and interest in the Licensed Software and
+ * Documentation.
  *
  * $Rev$
  * Last modified by $Author$
